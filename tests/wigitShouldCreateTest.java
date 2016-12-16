536baa372b94368f73e44eca8c73b378984c6f74
Jonathan Hunter <jhunter@subs-space.net>
2016-12-15 23:22:53 -0500

    Implemented a test!

---
+++ tests/wigitShouldCreateTest.java
@@ -0,0 +1,23 @@
+
+import org.junit.Test;
+import org.junit.Assert;
+import org.usfirst.frc.team342.jhunter.javatests.widgit;
+
+public class wigitShouldCreateTest
+{
+
+	@Test
+	public void testCreate()
+	{
+		widgit Testing = null;
+
+		Assert.assertNull(Testing);
+
+		Testing = new widgit();
+
+		Assert.assertNotNull(Testing);		
+
+	}
+
+
+}
